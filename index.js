cb61ea1f77373d47d0e568238e3670d7005b2c6b
Erin Egbert <erinegbert6@gmail.com>
2021-08-16 14:16:26 -0600

    require express

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const express = require('express'),
 morgan = require('morgan');
+
+const app = express();
+
 // Logging middleware (Morgan)
 app.use(morgan('common'));
 
@@ -65,3 +69,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
