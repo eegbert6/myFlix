b65a0bdd41049ef1bb8f5ab50807814733dcb1fc
Erin Egbert <erinegbert6@gmail.com>
2021-08-24 20:25:36 -0700

    removed old error handler

--- index.js
+++ index.js
@@ -267,12 +267,6 @@ app.delete('/users/:username/favorites/:MovieID', (req, res) => {
     });
 });
 
-// Error handler
-// app.use((err, req, res, next) => {
-//     console.log(err.stack);
-//     res.status(500).send('Something broke!');
-// });
-
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
