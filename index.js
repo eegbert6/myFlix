f5bef61863bb85b1d73bb5e67e3008ac964b67df
Erin Egbert <erinegbert6@gmail.com>
2021-08-25 08:23:44 -0700

    removed old code

--- index.js
+++ index.js
@@ -22,50 +22,6 @@ app.use(morgan('common'));
 // For sending static files
 app.use(express.static('public'));
 
-// An array of my top ten movies
-let topTenMovies = [
-    {
-        title: 'Coach Carter',
-        year: '2005'
-    },
-    {
-        title: 'Hunt for the Wilderpeople',
-        year: '2016'
-    },
-    {
-        title: 'How to Lose a Guy in 10 Days',
-        year: '2003'
-    },
-    {
-        title: 'You\'ve Got Mail',
-        year: '1998'
-    },
-    {
-        title: 'What Lies Beneath',
-        year: '2000'
-    },
-    {
-        title: 'Peanut Butter Falcon',
-        year: '2019'
-    },
-    {
-        title: 'Harry Potter: The Prisoner of Azkaban',
-        year: '2004'
-    },
-    {
-        title: 'Cinderella Story',
-        year: '2004'
-    },
-    {
-        title: 'Remember the Titans',
-        year: '2000'
-    },
-    {
-        title: 'Inception',
-        year: '2010'
-    }
-]
-
 // // Returning top ten movies
 // app.get('/movies', (req, res) => {
 //     res.json(topTenMovies);
