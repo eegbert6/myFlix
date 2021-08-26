8b4e10e4f5a186a4982934ecac4a5b859088a8f2
Erin Egbert <erinegbert6@gmail.com>
2021-08-26 07:49:19 -0700

    removed authorization from user registration

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get('/directors/:name', passport.authenticate('jwt', {session: false}), (req
     Birthday: Date
 }*/
 
-app.post('/users', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.post('/users', (req, res) => {
     Users.findOne({Username: req.body.Username})
         .then((user) => {
             if(user) {
