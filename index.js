f7a8cbf59c548b827bace656028e43585d67d062
Erin Egbert <erinegbert6@gmail.com>
2021-08-26 16:24:09 -0700

    installed and required cors

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const cors = require('cors');
+app.use(cors());
+
 let auth = require('./auth')(app);
 
 const passport = require('passport');
